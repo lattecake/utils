8d48b67529bd91df4c9beecd8e8d639ad2178751
congwang <congwang@creditease.cn>
2017-12-31 15:58:32 +0800

    change

--- common.go
+++ common.go
@@ -15,6 +15,7 @@ import (
 
 import (
 	"crypto/rand"
+	"net"
 )
 
 // 生成随机字符串
@@ -84,4 +85,23 @@ func Round(val float64, places int) float64 {
 	}
 
 	return t
+}
+
+// 本机ip地址
+func LocalAddress() string {
+	addrs, err := net.InterfaceAddrs()
+	if err == nil {
+		var ip string
+		for _, address := range addrs {
+			// 检查ip地址判断是否回环地址
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					ip += ipnet.IP.To4().String() + ","
+				}
+			}
+		}
+		ip = strings.TrimRight(ip, ",")
+		return ip
+	}
+	return "127.0.0.1"
 }
